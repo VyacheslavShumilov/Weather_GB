90690fd15b793310919ed1b966d9a7db3ab26cdf
bsod <aparin@alkor.co.ru>
2022-03-21 23:16:25 +0500

    lesson 6

--- app/src/main/java/com/als/gblesson2/MainActivity.kt
+++ app/src/main/java/com/als/gblesson2/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.als.gblesson2
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.als.gblesson2.databinding.ActivityMainBinding
+import com.als.gblesson2.expiriment.MainBroadCastReceiver
+import com.als.gblesson2.expiriment.ThreadsFragment
 import com.als.gblesson2.view.MainFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val receiver = MainBroadCastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commit()
+            openFragment(MainFragment.newInstance())
+//            openFragment(ThreadsFragment.newInstance())
         }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/als/gblesson2/view/DetailFragment.kt
+++ app/src/main/java/com/als/gblesson2/view/DetailFragment.kt
@@ -1,26 +1,41 @@
 package com.als.gblesson2.view
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.als.gblesson2.DetailsService
+import com.als.gblesson2.LATITUDE_EXTRA
+import com.als.gblesson2.LONGITUDE_EXTRA
 import com.als.gblesson2.R
+import com.als.gblesson2.data.FactDTO
 import com.als.gblesson2.data.Weather
 import com.als.gblesson2.data.WeatherDTO
-import com.als.gblesson2.data.WeatherLoader
 import com.als.gblesson2.databinding.FragmentDetailBinding
-import com.google.gson.Gson
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.MalformedURLException
-import java.net.URL
-import java.util.stream.Collectors
-import javax.net.ssl.HttpsURLConnection
+import com.als.gblesson2.expiriment.TestForeground
+
+const val DETAILS_INTENT_FILTER = "DETAILS INTENT FILTER"
+const val DETAILS_LOAD_RESULT_EXTRA = "LOAD RESULT"
+const val DETAILS_INTENT_EMPTY_EXTRA = "INTENT IS EMPTY"
+const val DETAILS_DATA_EMPTY_EXTRA = "DATA IS EMPTY"
+const val DETAILS_RESPONSE_EMPTY_EXTRA = "RESPONSE IS EMPTY"
+const val DETAILS_REQUEST_ERROR_EXTRA = "REQUEST ERROR"
+const val DETAILS_REQUEST_ERROR_MESSAGE_EXTRA = "REQUEST ERROR MESSAGE"
+const val DETAILS_URL_MALFORMED_EXTRA = "URL MALFORMED"
+const val DETAILS_RESPONSE_SUCCESS_EXTRA = "RESPONSE SUCCESS"
+const val DETAILS_TEMP_EXTRA = "TEMPERATURE"
+const val DETAILS_FEELS_LIKE_EXTRA = "FEELS LIKE"
+const val DETAILS_CONDITION_EXTRA = "CONDITION"
+private const val TEMP_INVALID = -100
+private const val FEELS_LIKE_INVALID = -100
+private const val PROCESS_ERROR = "Обработка ошибки"
 
 class DetailsFragment : Fragment() {
 
@@ -28,17 +43,36 @@ class DetailsFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var weatherBundle: Weather
 
-    private val loadListener  =
-        object : WeatherLoader.WeatherLoaderListener {
-            override fun onLoaded(weatherDTO: WeatherDTO) {
-                displayWeather(weatherDTO)
+    private val loadResultsReceiver: BroadcastReceiver = object :
+        BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.getStringExtra(DETAILS_LOAD_RESULT_EXTRA)) {
+                DETAILS_INTENT_EMPTY_EXTRA -> TODO(PROCESS_ERROR)
+                DETAILS_DATA_EMPTY_EXTRA -> TODO(PROCESS_ERROR)
+                DETAILS_RESPONSE_EMPTY_EXTRA -> TODO(PROCESS_ERROR)
+                DETAILS_REQUEST_ERROR_EXTRA -> TODO(PROCESS_ERROR)
+                DETAILS_REQUEST_ERROR_MESSAGE_EXTRA -> TODO(PROCESS_ERROR)
+                DETAILS_URL_MALFORMED_EXTRA -> TODO(PROCESS_ERROR)
+                DETAILS_RESPONSE_SUCCESS_EXTRA -> displayWeather(
+                    WeatherDTO(
+                        FactDTO(
+                            intent.getIntExtra(
+                                DETAILS_TEMP_EXTRA, TEMP_INVALID
+                            ),
+                            intent.getIntExtra(
+                                DETAILS_FEELS_LIKE_EXTRA,
+                                FEELS_LIKE_INVALID
+                            ),
+                            intent.getStringExtra(
+                                DETAILS_CONDITION_EXTRA
+                            )
+                        )
+                    )
+                )
+                else -> TODO(PROCESS_ERROR)
             }
-
-            override fun onFailed(throwable: Throwable) {
-                TODO("Not yet implemented")
-            }
-
         }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,22 +82,44 @@ class DetailsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        context?.registerReceiver(
+            loadResultsReceiver,
+            IntentFilter(DETAILS_INTENT_FILTER)
+        )
+    }
+
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         arguments?.getParcelable<Weather>(BUNDLE_EXTRA)?.let { weather ->
-           weatherBundle = weather
+            weatherBundle = weather
+
         }
-        binding.mainView.visibility = View.GONE
-        binding.loadingLayout.visibility = View.VISIBLE
-        loadWeather()
+        getWeather()
+        context?.let {
+            it.startForegroundService(Intent(it, TestForeground::class.java))
+        }
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun loadWeather() {
-        val weatherLoader = WeatherLoader(loadListener, weatherBundle.city.lat, weatherBundle.city.lon)
-        weatherLoader.loadWeather()
+    private fun getWeather() {
+        binding.mainView.visibility = View.GONE
+        binding.loadingLayout.visibility = View.VISIBLE
+        context?.let {
+            it.startService(Intent(it, DetailsService::class.java).apply {
+                putExtra(
+                    LATITUDE_EXTRA,
+                    weatherBundle.city.lat
+                )
+                putExtra(
+                    LONGITUDE_EXTRA,
+                    weatherBundle.city.lon
+                )
+            })
+        }
     }
+
 
     private fun displayWeather(weatherDTO: WeatherDTO) {
         with(binding) {
@@ -82,6 +138,10 @@ class DetailsFragment : Fragment() {
         }
     }
 
+    override fun onDestroy() {
+        context?.unregisterReceiver(loadResultsReceiver)
+        super.onDestroy()
+    }
 
 
     companion object {
